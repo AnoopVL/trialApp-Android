432efdc747d7e52c9048c522164d210db87d2dd1
AnoopVL <avlanjekar4@gmail.com>
2024-01-22 22:50:42 +0530

    New Activity added

--- app/src/main/java/com/avl/trialapp/MainActivity.java
+++ app/src/main/java/com/avl/trialapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.avl.trialapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonNavigate = findViewById(R.id.viewMore);
+        buttonNavigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the SecondActivity
+                Intent intent = new Intent(MainActivity.this, avl_prof.class);
+                startActivity(intent);
+            }
+        });
+
+//        Button buttonNavigate = findViewById(R.id.viewMore);
+//        buttonNavigate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, avl_prof.class);
+//                setIntent(intent);
+//            }
+//        });
+
     }
 }
