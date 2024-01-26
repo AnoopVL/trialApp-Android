010592649b8af0ccadb70cc5063e8a72674fb766
AnoopVL <avlanjekar4@gmail.com>
2024-01-27 01:10:45 +0530

    Instaram Intent

--- Assignment4/app/src/main/java/com/avl/assigment4/MainActivity.java
+++ Assignment4/app/src/main/java/com/avl/assigment4/MainActivity.java
@@ -1,16 +1,38 @@
 package com.avl.assigment4;
 
+import static android.app.PendingIntent.getActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button vstInstaBtn;
+    private Button mvQuzBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vstInstaBtn = findViewById(R.id.vstInstaBtn);
+        vstInstaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.instagram.com/anoop_lanjekar/";
+                    Uri webpage = Uri.parse(url);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
+
+            }
+        });
     }
 
-    private void setContentView(int activityMain) {
-    }
+
 }
