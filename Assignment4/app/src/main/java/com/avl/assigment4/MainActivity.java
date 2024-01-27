51ed93301e8970bb6f49dde0956236800b39d6ac
AnoopVL <avlanjekar4@gmail.com>
2024-01-27 17:53:21 +0530

    Intent directly opens in relative app

--- Assignment4/app/src/main/java/com/avl/assigment4/MainActivity.java
+++ Assignment4/app/src/main/java/com/avl/assigment4/MainActivity.java
@@ -4,11 +4,13 @@ import static android.app.PendingIntent.getActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button vstInstaBtn;
@@ -21,14 +23,32 @@ public class MainActivity extends AppCompatActivity {
 
         vstInstaBtn = findViewById(R.id.vstInstaBtn);
         vstInstaBtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("QueryPermissionsNeeded")
             @Override
             public void onClick(View v) {
-                String url = "https://www.instagram.com/anoop_lanjekar/";
-                    Uri webpage = Uri.parse(url);
-                    Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-                    if (intent.resolveActivity(getPackageManager()) != null) {
-                        startActivity(intent);
-                    }
+                    //Directly opens the website in relative app
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.instagram.com/anoop_lanjekar/"));
+                startActivity(intent);
+
+//                String url = "https://www.instagram.com/anoop_lanjekar";
+//                Uri uri = Uri.parse(url);
+//                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//
+//// Check if there's a web browser available to handle the intent
+//                if (intent.resolveActivity(getPackageManager()) != null) {
+//                    startActivity(intent);
+//                } else {
+//                    // Handle the case where no web browser is available
+//                    Toast.makeText(MainActivity.this, "Now web browser found", Toast.LENGTH_SHORT).show();
+//                }
+
+//                String url = "https://www.instagram.com/anoop_lanjekar/";
+//                    Uri webpage = Uri.parse(url);
+//                    Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+//                    if (intent.resolveActivity(getPackageManager()) != null) {
+//                        startActivity(intent);
+//                    }
 
             }
         });
